226fbe7e29fee8c4744efaa2b94e2aa820e657d6
morjokill <morjokill@gmail.com>
2022-04-22 21:27:09 +0300

    Init project

---
+++ src/main/java/com/morj/oauth2/code/flow/OAuth2CodeFlowApplication.java
@@ -0,0 +1,13 @@
+package com.morj.oauth2.code.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2CodeFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2CodeFlowApplication.class, args);
+    }
+
+}
